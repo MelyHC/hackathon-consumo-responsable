da11f7fd8d906e2e76955d0e66eacb7da6f07e7a
MelyHC <mely.hidalgo.crespo@gmail.com>
2019-03-17 09:49:29 -0500

    map

--- src/map.js
+++ src/map.js
@@ -1,37 +1,17 @@
-const createMarker = (place) => {
-  // Creamos un marcador
-  const marker = new google.maps.Marker({
-    map: map,
-    position: place.coords
-  });
-
-  // Asignamos el evento click del marcador
-  google.maps.event.addListener(marker, 'click', () => {
-    infowindow.setContent(place.name);
-    infowindow.open(map);
-  });
-}
-
-let map;
-let infowindow;
-
+const googleMap = document.getElementById('map');
+// console.log(googleMap.src ='hola')
 window.onload = () => {
-  // Creamos un mapa con las coordenadas actuales
-  navigator.geolocation.getCurrentPosition(pos => {
+  const validateLocation = Object.keys(navigator.geolocation)
+  if (validateLocation.length) {
+    // Creamos un mapa con las coordenadas actuales
+    navigator.geolocation.getCurrentPosition(pos => {
 
-    lat = pos.coords.latitude;
-    lon = pos.coords.longitude;
+      lat = pos.coords.latitude;
+      lon = pos.coords.longitude;
 
-    const myLatlng = new google.maps.LatLng(lat, lon);
-    const mapOptions = {
-      center: myLatlng,
-      zoom: 15,
-    };
-
-    map = new google.maps.Map(googleMap, mapOptions);
-
-    // Creamos el infowindow
-    infowindow = new google.maps.InfoWindow();
-    markerRest.forEach(colocationMarker => createMarker(colocationMarker))
-  });
+      googleMap.src = `https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d31205.143386384454!2d${lat}!3d${lon}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sreciclaje+!5e0!3m2!1ses-419!2spe!4v1552832477606`
+    });
+  } else {    
+    googleMap.src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d31205.143386384454!2d-77.00885709517585!3d-12.136566264291682!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sreciclaje+!5e0!3m2!1ses-419!2spe!4v1552832477606"
+  }
 }
